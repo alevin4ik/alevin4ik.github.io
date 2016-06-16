ae7269b55f2504c721802fdf88cd5e11bed2959f
alevin <alevin4ik@hotmail.com>
2016-06-16 22:56:02 +0300

    v4

--- game/21/21.js
+++ game/21/21.js
@@ -2,6 +2,7 @@
  * Created by alevin on 16.06.16.
  */
 var Help_list, Help_str0, Help_str1, Help_str2,  Help_str3, Help_str4;
+var playername, x;
 
 Help_str0 = "<h2>Правила игры в 21 :<h2>";
 Help_str1 = "<p>1. Задача набрать число очков приближенное к 21, но не привышающее 21.</p>";
@@ -15,7 +16,13 @@ function HelpToGame() {
     document.getElementById('game_set').innerHTML = Help_list + "<div id=\"CenterButton\"><button id=\"StartButton\" onclick=\"style.display='none';StartToGame()\">Начать</button></div>";
 }
 
+function GiveName() {
+    playername = document.getElementById("Name").value;
+    alert("Поздравляю игрок " + playername + "!")
+}
+
 function StartToGame() {
-    var playername, x;
-    /*<input type="text" placeholder="Имя набирать тут" id="name_tag" >*/
+    document.getElementById('game_set').innerHTML = "";
+    document.getElementById('game_set').innerHTML += "<input id=\"Name\" type=\"text\" placeholder=\"Имя набирать тут\" id=\"name_tag\">";
+    document.getElementById('game_set').innerHTML += "<button id=\"StartButton\" onclick=\"style.display='none';GiveName()\">Принять</button>";
 }
