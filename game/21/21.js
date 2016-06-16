e28c3ce77e24b8413db853e746d943c7fbcc4f83
alevin <alevin4ik@hotmail.com>
2016-06-16 22:18:39 +0300

    v3.8

--- game/21/21.js
+++ game/21/21.js
@@ -1,12 +1,18 @@
 /**
  * Created by alevin on 16.06.16.
  */
-var text1;
-text1 = "<p>Дамка</p>";
-document.getElementById('game_set').innerHTML += text1 + "\n";
+var Help_list, Help_str0, Help_str1, Help_str2,  Help_str3, Help_str4;
 
-function StartGame() {
-    document.getElementById('game_set').innerHTML += text1 + "\n"+ "<button onclick=\"style.display='none';StartGame()\">Нажми</button>";
+Help_str0 = "<h2>Правила игры в 21 :<h2>";
+Help_str1 = "<p>1. Задача набрать число очков приближенное к 21, но не привышающее 21.</p>";
+Help_str2 = "<p>2. Играют одной колодой в 36 карт.</p>";
+Help_str3 = "<p>3. При подсчете очков старшинство карт не стандартно.<br>(Валет - 2 очка; Дама - 3 очка; Король - 4 очка; Туз - 11 очков </p>";
+Help_str4 = "<p></p>";
+
+Help_list = Help_str0 + Help_str1 + Help_str2 + Help_str3 + Help_str4;
+
+function HelpToGame() {
+    document.getElementById('game_set').innerHTML += Help_list + "<div id=\"CenterButton\"><button id=\"HelpButton\" onclick=\"style.display='none';HelpToGame()\">Правила</button></div>";
 }
 
 function Stop(text) {
