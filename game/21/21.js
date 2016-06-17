f126bd819e932998159689f5c38ee2f36755af68
alevin <alevin4ik@hotmail.com>
2016-06-17 11:33:52 +0300

    v4.5

--- game/21/21.js
+++ game/21/21.js
@@ -3,7 +3,7 @@
  */
 var Help_list, Help_str0, Help_str1, Help_str2,  Help_str3, Help_str4;
 var playername, playerpoint = 0, machinpoint, pointRand, randCard;
-var printCard ;
+var printCard, bordpoint;
 
 Help_str0 = "<h2>Правила игры в 21 :<h2>";
 Help_str1 = "<p>1. Задача набрать число очков приближенное к 21, но не привышающее 21.</p>";
@@ -31,61 +31,84 @@ function PreStat() {
 
 function card_set() {
     if (randCard == 0){
-        printCard = printCard + "Diamonds.png\">";
+        printCard = printCard + "Diamonds.png\"></div>";
     }else if (randCard == 1){
-        printCard = printCard + "Hearts.png\">";
+        printCard = printCard + "Hearts.png\"></div>";
     }else if (randCard == 2){
-        printCard = printCard + "Spades.png\">";
+        printCard = printCard + "Spades.png\"></div>";
     }else if (randCard == 3){
-        printCard = printCard + "Clubs.png\">";
+        printCard = printCard + "Clubs.png\"></div>";
     }
 }
 
 function point_random() {
     if (pointRand == 0){
-        printCard = "<image src=\"card/11/";
+        printCard = "<div id=\"img\"><image src=\"card/11/";
         card_set();
         playerpoint = playerpoint + 11;
     }else if (pointRand == 1){
-        printCard = "<image src=\"card/10/";
+        printCard = "<div id=\"img\"><image src=\"card/10/";
         card_set();
         playerpoint = playerpoint + 10;
     }else if (pointRand == 2){
-        printCard = "<image src=\"card/9/";
+        printCard = "<div id=\"img\"><image src=\"card/9/";
         card_set();
         playerpoint = playerpoint + 9;
     }else if (pointRand == 3){
-        printCard = "<image src=\"card/8/";
+        printCard = "<div id=\"img\"><image src=\"card/8/";
         card_set();
         playerpoint = playerpoint + 8;
     }else if (pointRand == 4){
-        printCard = "<image src=\"card/7/";
+        printCard = "<div id=\"img\"><image src=\"card/7/";
         card_set();
         playerpoint = playerpoint + 7;
     }else if (pointRand == 5){
-        printCard = "<image src=\"card/6/";
+        printCard = "<div id=\"img\"><image src=\"card/6/";
         card_set();
         playerpoint = playerpoint + 6;
     }else if (pointRand == 6){
-        printCard = "<image src=\"card/4/";
+        printCard = "<div id=\"img\"><image src=\"card/4/";
         card_set();
         playerpoint = playerpoint + 4;
     }else if (pointRand == 7){
-        printCard = "<image src=\"card/3/";
+        printCard = "<div id=\"img\"><image src=\"card/3/";
         card_set();
         playerpoint = playerpoint + 3;
     }else if (pointRand == 8){
-        printCard = "<image src=\"card/2/";
+        printCard = "<div id=\"img\"><image src=\"card/2/";
         card_set();
         playerpoint = playerpoint + 2;
     }
 }
 
+function CloseGame() {
+    document.getElementById('game_set').innerHTML = "";
+    if (playerpoint == 21){
+        document.getElementById('game_set').innerHTML = "<b>Вы победили!</b>";
+    }
+}
+
+function bord_name() {
+    bordpoint = "<div id=\"bord_point\"><b>Колличество очков игрока " + playername + " : ";
+}
+
+function Nv() {
+    if (playerpoint == 21){
+        CloseGame();
+    }else if (playerpoint > 21){
+        CloseGame();
+    }
+}
+
 function StartToGame() {
+    bord_name();
+    document.getElementById('game_set').innerHTML = "";
     pointRand = Math.floor(Math.random()*8);
     randCard = Math.floor(Math.random()*3);
     point_random();
-    document.getElementById('game_set').innerHTML += printCard;
-    document.getElementById('game_set').innerHTML += "<button id=\"Me\" onclick=\"style.display='none';StartToGame()\">Взять</button>"
-    document.getElementById('game_set').innerHTML += "<button id=\"Pass\" onclick=\"style.display='none';CloseGame()\">Пас</button>"
+    bordpoint = bordpoint + playerpoint + "</b></div>";
+    document.getElementById('game_set').innerHTML += bordpoint + "<br>";
+    document.getElementById('game_set').innerHTML += printCard + "<br>";
+    document.getElementById('game_set').innerHTML += "<div id=\"Me\"><button onclick=\"style.display='none';StartToGame()\">Взять</button><button onclick=\"style.display='none';CloseGame()\">Пас</button></div>";
+    Nv();
 }
